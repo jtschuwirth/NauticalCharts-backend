598de0407c71858b4a99291805c93389f07cb325
jtschuwirth <jtschuwirth@uc.cl>
2021-11-24 14:41:28 -0300

    migracion funciones al backend

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ class Queue {
 
             socket.on("foundGame", (data) => {
                 console.log(`${data.player} found game`);
-                this.partida(socket, data.id, data.players);
+                this.partida(socket, data.id, data.player, data.players);
             });
         });
     }
@@ -57,7 +57,7 @@ class Queue {
             io.to(room).emit("statusQueue", {players: players});
             
             //quitamos al jugador de la Queue
-            for (let i=0; i<players; i++) {
+            for (let i=0; i<players.length; i++) {
                 var index = this.users.indexOf(players[i]);
                 this.users.splice(index,1);
                 console.log(this.users);
@@ -65,10 +65,17 @@ class Queue {
         }
     }
 
-    partida(socket, id, players) {
+    partida(socket, id, player, players) {
         socket.join(id);
         console.log(`partida iniciada ${id}`)
         io.to(id).emit("partida", `Partida iniciada id: ${id}`);
+        let dices = this.rollDices();
+        let pos = this.pos_inicial();
+        let map = this.crearMapa();
+        io.to(id).emit("startInfo", {
+            dices: dices, 
+            pos: pos,
+            map: map});
 
         let endedTurn = [];
         socket.on("endTurn", (data) => {
@@ -78,10 +85,84 @@ class Queue {
                 endedTurn.push(data.userAddress)
             }
             if (endedTurn.length == players.length) {
-                io.to(id).emit("endTurn", "New Round");
+                console.log("New Round");
+                let dices = this.rollDices();
+                io.to(id).emit("newRound", {dices: dices});
             }
         });
+    }
 
+    rollDices() {
+        const dice0 = Math.floor(Math.random() * (7 - 1)) + 1;
+        const dice1 = Math.floor(Math.random() * (7 - 1)) + 1;
+        const dice2 = Math.floor(Math.random() * (7 - 1)) + 1;
+        return [dice0, dice1, dice2]
+    }
+
+    crearMapa() {
+        const size_x = 13;
+        const size_y = 13;
+        var n_isles = 10;
+        const min_dis = 3;
+        const loot_min = 2;
+        const loot_max = 10;
+        //creamos el array con valores (100 - agua) y el tamaño correcto
+        var mapa = Array.apply(null, Array(size_y)).map( () => {
+            return Array.apply(null, Array(size_x)).map( () => {return 100} )
+        });
+      
+        //las islas no pueden estar en los límites del mapa
+        var pos_islas = []
+        for (var i = 1; i < size_x - 1; i++) {
+          for (var j = 1; j < size_y - 1; j++) {
+            pos_islas.push({x: i, y: j});
+          }
+        }
+        this.shuffle(pos_islas)
+      
+        //asignamos las islas en el tablero
+        while (pos_islas.length > 0 && n_isles > 0){
+            var pos = pos_islas.pop();
+            mapa[pos.y][pos.x] = this.getRandomInt(loot_min, loot_max);
+            n_isles--;
+          
+      
+            var temp = [];
+            for (var i = 1; i < pos_islas.length; i++){
+                if ( this.dist( pos_islas[i], pos) > min_dis ){
+                temp.push(pos_islas[i])
+                }
+            }
+            pos_islas = temp
+        }
+      
+      //{x: xVal, y: yVal}
+      
+        return mapa;
+    }
+
+    pos_inicial() {
+        var pos = [0,0,0];
+        return pos
+    }
+
+    dist(coord_1, coord_2) {
+        return Math.abs(coord_1.x - coord_2.x) + Math.abs(coord_1.y - coord_2.y)
+    }
+      
+    shuffle(array) {
+        //https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+    }
+      
+    getRandomInt(min, max) {
+        //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
 }
